7ea22dea3dc39e2d529d866fb0dfff7c6c5a9ae7
coloredscratch <84486011+coloredscratch@users.noreply.github.com>
2025-02-04 20:02:35 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+// Get the button element (assuming it has an ID of "myButton")
+const button = document.getElementById('alertin');
+
+// Add an event listener for the click event
+button.addEventListener('click', () => {
+  // Code to execute when the button is clicked
+  alert('Button clicked!'); 
+  // You can replace this with any other action you want to perform
+});
